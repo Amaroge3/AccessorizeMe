a6bb83fd6075dd10352fe4186949257f3cb75618
Andi Maroge <Amaroge3@gmail.com>
2019-01-23 05:02:42 -0800

    Added initializers and prepareForReuse.

--- AccessorizeMe/AccessoryArtViewController/AccessoryCollectionViewCell.swift
+++ AccessorizeMe/AccessoryArtViewController/AccessoryCollectionViewCell.swift
@@ -12,4 +12,15 @@ class AccessoryCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var accessoryImage: UIImageView!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
 }
+
