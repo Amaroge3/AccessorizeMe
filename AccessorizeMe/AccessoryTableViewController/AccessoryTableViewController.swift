19bf4acbf8a3017fb664548ebcc8e12da7d3a3bf
Andi Maroge <Amaroge3@gmail.com>
2019-01-25 02:15:02 -0800

    Code cleanup.

--- AccessorizeMe/AccessoryTableViewController/AccessoryTableViewController.swift
+++ AccessorizeMe/AccessoryTableViewController/AccessoryTableViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class AccessoryTableViewController: UITableViewController {
-
+    
     lazy var accessoryTableViewModel = AccessoryTableViewModel()
     var accessoryTableViewDocument: [String]?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,45 +20,45 @@ class AccessoryTableViewController: UITableViewController {
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
-
+    
     /**
-        # Number of Sections
-    */
+     # Number of Sections
+     */
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     /**
      # Number of Rows In Section
-    */
+     */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return accessoryTableViewDocument!.count
     }
-
+    
     /**
      # Cell for Row At
-        The table view function retrieves the custom cell and assigns the accessory types to the cell's
-        text label.
-    */
+     The table view function retrieves the custom cell and assigns the accessory types to the cell's
+     text label.
+     */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AccessoryCell", for: indexPath)
-            cell.textLabel?.text = accessoryTableViewDocument![indexPath.row]
+        cell.textLabel?.text = accessoryTableViewDocument![indexPath.row]
         
         return cell
     }
     /**
      # TableView DidSeletRowAt
-        TableView Function that shows the split view MVC and sends the accessory type the user selects
-        from the table view.
- */
+     TableView Function that shows the split view MVC and sends the accessory type the user selects
+     from the table view.
+     */
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let accessoryType = accessoryTableViewDocument![indexPath.row]
         if let split = self.splitViewController {
@@ -70,58 +70,58 @@ class AccessoryTableViewController: UITableViewController {
         toggleAnimatedHiddenSplitViewController()
         
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 extension UITableViewController {
     /**
      # Toggle Animated Split View Controller
-        Hides the split view controller with animation.
-    */
+     Hides the split view controller with animation.
+     */
     func toggleAnimatedHiddenSplitViewController() {
         if view.traitCollection.userInterfaceIdiom == .pad && splitViewController?.displayMode == .primaryOverlay {
             let animations: () -> Void = {
